2e0cdaef62b4bd1c187e9aabe8fae67ecdbd61d2
DESKTOP-95F3B96\dheeraj <dheerajb412@gmail.com>
2019-12-10 23:18:04 -0500

    BeanScope ProtoType

---
+++ src/main/java/com/spring/basics/springbasics/scope/PersonDAO.java
@@ -0,0 +1,19 @@
+package com.spring.basics.springbasics.scope;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class PersonDAO {
+
+    @Autowired
+    JdbcConnection jdbcConnection;
+
+    public JdbcConnection getJdbcConnection() {
+        return jdbcConnection;
+    }
+
+}
